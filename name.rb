5c2309379992d76f9ae2ed70215cc5cabd64c283
Josh Ford <jford4@lesley.edu>
2014-11-07 22:28:47 +0000

    variables

---
+++ name.rb
@@ -0,0 +1,8 @@
+puts 'Please enter your first name:'
+first_name = gets.chomp
+puts 'Please enter your last name:'
+last_name = gets.chomp
+puts first_name + ' ' + last_name + ', ' 'huh? That is a poor name. You must be a doofus! I mock you now!'
+10.times do
+  puts first_name + ' ' + last_name
+end
